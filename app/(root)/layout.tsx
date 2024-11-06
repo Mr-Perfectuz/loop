b11c1934d14bd5f467c40462aaf60747167f9ec3
Anvar <ziyodovanvar1999@gmail.com>
2024-11-06 21:30:07 +0900

    Feat: develop Navbar and Sidebar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,6 @@ import React, { ReactNode } from 'react';
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main>
-      <section>Navbar</section>
       {children}
       <section>Footer</section>
     </main>
